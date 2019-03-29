7868156458d651e651b2c946847838f87738a903
kanitamago <kanitamago5503@gmail.com>
2019-03-29 23:38:46 +0900

    posting-source-code: DATABASE_URL update

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 import os
-os.environ["DATABASE_URL"] = "postgres://edsehmgghzeyen:9104b82860c4e3f704698ea52b7b13e096d69ea794d68cbab09a85e6587ebacb@ec2-107-20-177-161.compute-1.amazonaws.com:5432/d920p1snhobdo3"
+os.environ["DATABASE_URL"] = "postgres://hitzsqrrbfjzdp:53c3f20bca566bc9a96681e085cd8dd2af5d44eacd5da25ca620832e61e4db1a@ec2-54-225-129-101.compute-1.amazonaws.com:5432/deucp02ml0a000"
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 USERNAME = "kanitamago6653"
 PASSWORD = "1996tamago"
-SECRET_KEY = "show_code1996kanitamago"
+SECRET_KEY = "posting_source_code1996kanitamago"
 DEBUG = True
