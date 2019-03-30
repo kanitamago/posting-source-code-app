8d053b43d9b0127f32efc07ea7bb2019f46f4157
kanitamago <kanitamago5503@gmail.com>
2019-03-30 12:25:24 +0900

    posting-source-code: config.py hidden

--- app/config.py
+++
@@ -1,7 +0,0 @@
-import os
-os.environ["DATABASE_URL"] = "postgres://hitzsqrrbfjzdp:53c3f20bca566bc9a96681e085cd8dd2af5d44eacd5da25ca620832e61e4db1a@ec2-54-225-129-101.compute-1.amazonaws.com:5432/deucp02ml0a000"
-SQLALCHEMY_TRACK_MODIFICATIONS = True
-USERNAME = "kanitamago6653"
-PASSWORD = "1996tamago"
-SECRET_KEY = "posting_source_code1996kanitamago"
-DEBUG = True
